2deee06c07e864be0b82bfc1c659f30380e8734d
unknown <ak00se12k16@gmail.com>
2020-03-12 17:41:36 +0530

    bootstrapped

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div class="d-md-flex h-md-100 align-items-center">
+
+          <div class="col-md-6 p-0 bg-indigo h-md-100">
+              <div class="text-white d-md-flex align-items-center h-100 p-5 text-center justify-content-center">
+                  <div class="logoarea pt-5 pb-5">
+                      First half content here
+                  </div>
+              </div>
+          </div>
+
+          <div class="col-md-6 p-0 bg-white h-md-100 loginarea">
+              <div class="d-md-flex align-items-center h-md-100 p-5 justify-content-center">
+                  Second half content here
+              </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
