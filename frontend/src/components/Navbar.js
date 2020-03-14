c7be73bb822e72344a92e243b631cdf69dbd2cac
unknown <ak00se12k16@gmail.com>
2020-03-14 16:38:01 +0530

    navbar changed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,31 +8,8 @@ class Navbar extends Component{
         this.props.history.push(`/`)
     }
     render(){
-        const loginRegLink = (
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <Link to="/login" className="nav-link">
-                        Login
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/register" className="nav-link">
-                        Register
-                    </Link>
-                </li>
-            </ul>
-        )
         const userLink = (
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <a href="" onClick={this.logOut.bind(this)} className="nav-link">
-                        Logout
-                    </a>
-                </li>
-            </ul>
-        )
 
-        return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded">
 
                 <button 
@@ -53,16 +30,26 @@ class Navbar extends Component{
                     id="navbar1">
 
                     <ul className="navbar-nav">
+
                         <li className="nav-item">
                             <Link to="/profile" className="nav-link">
                                 Home
                             </Link>
                         </li>
+
+                        <li className="nav-item">
+                            <a href="" onClick={this.logOut.bind(this)} className="nav-link">
+                                Logout
+                            </a>
+                        </li>
                     </ul>
-                    {localStorage.usertoken ? userLink : loginRegLink}
                 </div>
 
             </nav>
+        )
+
+        return (
+            localStorage.usertoken ? userLink : null
         )
     }
 }
