ee4f29c89a44468e86a2558ec6cdc576e547a5b6
unknown <ak00se12k16@gmail.com>
2020-03-14 16:06:21 +0530

    table commit

--- frontend/src/components/Events1.js
+++ frontend/src/components/Events1.js
@@ -73,21 +73,44 @@ class Events1 extends Component {
 
           <div class="col-md-6 p-0 bg-white h-md-100 loginarea">
               <div class="d-md-flex align-items-center h-md-100 p-5 justify-content-center">
-              <form class="border rounded p-5">
-				<h3 class="mb-4 text-center">Sign In</h3>
-				<div class="form-group">
-					<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail" required="" />
-				</div>
-				<div class="form-group">
-					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required="" />
-				</div>
-				<div class="form-group form-check">
-					<input type="checkbox" class="form-check-input" id="exampleCheck1" />
-					<label class="form-check-label small text-muted" for="exampleCheck1">Remember me</label>
-				</div>
-				<button type="submit" class="btn btn-success btn-round btn-block shadow-sm">Sign in</button>
-				<small class="d-block mt-4 text-center"><a class="text-gray" href="#">Forgot your password?</a></small>
-			</form>
+              <div class="table-wrapper-scroll-y my-custom-scrollbar">
+
+<table class="table table-bordered table-striped mb-0">
+  <thead>
+    <tr>
+      <th scope="col">SOID</th>
+      <th scope="col">Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">GS01</th>
+      <td>5CSE1<br/>27/03/2020/<br/>3</td>
+    </tr>
+    <tr>
+      <th scope="row">GS01</th>
+      <td>5CSE1<br/>27/03/2020/<br/>3</td>
+    </tr>
+    <tr>
+      <th scope="row">GS01</th>
+      <td>5CSE1<br/>27/03/2020/<br/>3</td>
+    </tr>
+    <tr>
+      <th scope="row">GS01</th>
+      <td>5CSE1<br/>27/03/2020/<br/>3</td>
+    </tr>
+    <tr>
+      <th scope="row">GS01</th>
+      <td>5CSE1<br/>27/03/2020/<br/>3</td>
+    </tr>
+    <tr>
+      <th scope="row">GS01</th>
+      <td>5CSE1<br/>27/03/2020/<br/>3</td>
+    </tr>
+  </tbody>
+</table>
+
+</div>
               </div>
           </div>
         </div>
