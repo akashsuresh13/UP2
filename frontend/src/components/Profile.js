5c239c59eab1a13a847f2665d2413890b3f42b42
unknown <ak00se12k16@gmail.com>
2020-03-16 18:03:10 +0530

    substitute_tinkering

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,54 +1,60 @@
 import React, { Component } from 'react'
 import jwt_decode from 'jwt-decode'
 import { Link,withRouter } from 'react-router-dom'
+import substitute_icon from '../images/icons/substitute.png'
+import '../App.css'
+import profile_icon from '../images/icons/profile.png'
 // import { decode } from 'punycode'
 
 export default class Profile extends Component{
 
+    constructor(){
+        super()
+        this.state = {
+            userID: '',
+            userFID: '',
+            userName: '',
+            reload: false
+        }
+    }
+
+    componentDidMount(){
+        let token = localStorage.usertoken
+        let decoded = jwt_decode(token)
+        this.setState({
+            userID: decoded.user_email_id,
+            userFID: decoded.user_FID,
+            userName: decoded.user_name
+        })
+    }
+
     render(){
 
         return(
-<<<<<<< HEAD
             <div class="d-md-flex h-md-100 align-items-center">
                 <div class="col-md-6 p-0 bg-indigo h-md-100">
-                    First Content
+                    {/* <div class = "mytooltip">
+                        <img src={profile_icon} class="img-fluid" alt="Responsive image"></img>
+                        <div class = "mytext">
+                            <ul>
+                                <li>{this.state.userName}</li>
+                                <li>{this.state.userID}</li>
+                                <li>{this.state.userFID}</li>
+                            </ul>
+                        </div>
+                    </div>                         */}
+                    <div class="text-white d-md-flex align-items-center h-100 p-5 text-center justify-content-center">
+                            <Link to="/subs_raise" className="nav-link">
+                                <div class = "mytext">R</div>
+                            </Link>
+                            <img src={substitute_icon} class="img-fluid" alt="Responsive image"></img>
+                            <Link to="/subs_offer" className="nav-link">
+                                <div class = "mytext">O</div>
+                            </Link>
+                    </div>
                 </div>
                 <div class="col-md-6 p-0 bg-white h-md-100 loginarea">
                     Second Content
-=======
-            <div className="container">
-                <div className="jumbotron mt-5">
-                    <div className="col-sm-8 mx-auto">
-                        <h1 className="text-center">PROFILE</h1>
-                    </div>
-                    <table className="table col-md-6 mx-auto">
-                        <tbody>
-                            <tr>
-                                <td>First Name</td>
-                                <td>{this.state.userID}</td>
-                            </tr>
-                            <tr>
-                                <td>Last Name</td>
-                                <td>{this.state.userFID}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <li className="nav-item">
-                        <Link to="/events" className="nav-link">
-                            Events
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/subs_raise" className="nav-link">
-                            Substitute Raise
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/subs_offer" className="nav-link">
-                            Substitute Offer
-                        </Link>
-                    </li>
->>>>>>> e2d7e2662361cd319f1c64de3d3af31867199ba3
                 </div>
             </div>
         )
--- frontend/src/components/Subs_Raise.js
+++ frontend/src/components/Subs_Raise.js
@@ -33,49 +33,71 @@ export default class Subs_Raise extends Component {
               <div class="text-white d-md-flex align-items-center h-100 p-5 text-center justify-content-center">
                 <div class="border rounded p-5">
                 
-                  <div className="form-group">
-                    <div class="form-inline">
-                      <div class="form-group">
-                        <DatePicker 
-                          placeholderText="Start Date"
-                          dateFormat="d-MM-yyyy"
-                          class="form-control"
-                          value={this.state.startDate}
-                          onChange={this.onChange}
-                        />
-                        <img src={calendar_icon} class="img-fluid" alt="Responsive image"></img>
-                      </div>
-                      <div class="form-group">
-                        <DatePicker 
-                          placeholderText="End Date"
-                          dateFormat="d-MM-yyyy"
-                          class="form-control"
-                          value={this.state.endDate}
-                          onChange={this.onChange}
-                        />
-                        <img src={calendar_icon} class="img-fluid" alt="canlendar icon"></img>
-                      </div>
-                      <div class="form-group">
-                        <select id="hr_no">
-                          <option value="default">Select Hour</option>
-                          <option value="1">L1</option>
-                          <option value="2">T3</option>
-                          <option value="3">T5</option>
-                          <option value="4">T2</option>
-                        </select> 
-                        <div class="form-group">
-                        <input type="radio" value="entire_day" id="entire_day" name="entire_day"></input>  
-                        <label for="entire_day">Entire Day</label>
-                        </div>
-                      </div>
-                      
-                    </div>
-                  </div> 
-                  <div className="form-group">
-                      <button type="button" class="button-margin btn btn-primary">Add</button>
-                      <button type="button" class="button-margin btn btn-danger">Delete</button>
+
+                <div class="form-group"><div class="input-group">
+                  <div class="input-group-prepend">
+                    <img src={calendar_icon} class="img-fluid" alt="canlendar icon"></img>
                   </div>
+                  <DatePicker 
+                    placeholderText="Start Date"
+                    dateFormat="d-MM-yyyy"
+                    value={this.state.startDate}
+                    selected={this.state.startDate}
+                    name="startDate"
+                    onChange={(date) => {this.setState({startDate:date})}}
+                  />
+                </div></div>
+
+                <div class="form-group"><div class="input-group">
+                  <div class="input-group-prepend">
+                    <img src={calendar_icon} class="img-fluid" alt="canlendar icon"></img>
+                  </div>
+                  <DatePicker 
+                    placeholderText="End Date"
+                    dateFormat="d-MM-yyyy"
+                    name="endDate"
+                    value={this.state.endDate}
+                    selected={this.state.endDate}
+                    onChange={(date) => {this.setState({endDate:date})}}
+                  />
+                </div></div>
+
+                <div class="form-group">
+                  <select id="hr_no" class="custom-select">
+                    <option value="default">Select Hour</option>
+                    <option value="1">L1</option>
+                    <option value="2">T3</option>
+                    <option value="3">T5</option>
+                    <option value="4">T2</option>
+                  </select> 
                 </div>
+
+                <div class="form-group"><div class="input-group">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">
+                      <input type="checkbox" aria-label="Checkbox for following text input" />
+                    </div>
+                  </div>
+                  <input type="text" class="form-control" value="Entire Day" readOnly/>
+                </div></div>
+
+                  {/* <div className="form-group">
+                    <button type="button" class="btn btn-primary btn-lg btn-block">ADD</button>
+                  </div>
+
+                  <div className="form-group">
+                    <button type="button" class="btn btn-danger btn-lg btn-block">DELETE</button>
+                  </div> */}
+
+                  <div className="form-group">
+                    <div class="btn-group btn-group-lg" role="group" aria-label="Basic example">
+                      <button type="button" class="btn btn-danger">DELETE</button>
+                      <button type="button" class="btn btn-primary">ADD</button>
+                      <button type="button" class="btn btn-warning">RESET</button>
+                    </div>
+                  </div>
+
+                  </div>
               </div>
           </div>
 
